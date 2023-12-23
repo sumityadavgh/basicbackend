9de49594e274613d09ef147b96dd3808a5c76c9d
Sumit Yadav <sumitydv8416@gmail.com>
2023-12-24 00:20:53 +0530

    cloudinary setup completed

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,8 @@
 const asyncHandler = (requestHandler) => {
-    Promise.resolve(requestHandler(req, res, next))
-    .catch((err) => next(err))
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
 }
+
+
+export { asyncHandler }
