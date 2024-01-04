a4e03f4ebdcbd6066ad98f090029ac09019835eb
Sumit Yadav <sumitydv8416@gmail.com>
2024-01-04 19:14:01 +0530

    refresh token routes created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -230,4 +230,4 @@ const refreshAccessToken = asyncHandler( async(req, res) => {
 } )
 
 
-export { registerUser, loginUser, logoutUser}
+export { registerUser, loginUser, logoutUser, refreshAccessToken}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { 
+    registerUser,
+    loginUser, 
+    logoutUser,
+    refreshAccessToken } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -20,9 +24,11 @@ router.route("/register").post(
     registerUser
     )
 
-    router.route("/login").post(loginUser)
+    router.route("/login").post(loginUser);
 
-    router.route("/logout").post(verifyJWT , logoutUser)
+    //secured Routes
+    router.route("/logout").post(verifyJWT , logoutUser);
+    router.route("/refresh-token").post(refreshAccessToken);
 
 
 export default router
